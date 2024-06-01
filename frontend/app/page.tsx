6a3a612daa15db08414e32cd981c5041a08dc589
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-01 12:33:36 -0300

    add frontend with nextjs

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className='max-w-4xl mx-auto mt-4 bg-red-400'>
+      <div className='text-center my-5 flex flex-col gap-4'>
+        <h1 className="text-2xl font-bold">Movies</h1>
+      </div>
+    </main>
+  );
+}
