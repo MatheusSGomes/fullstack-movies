1ab0d0fedfac0874e2574e4ba24f674967c62250
Matheus Gomes <math.s.gomes@gmail.com>
2024-06-01 20:36:22 -0300

    feat: add navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,12 +7,26 @@ export default async function Home() {
   const users = await getAllUsers();
 
   return (
-    <main className='max-w-4xl mx-auto mt-4'>
-      <div className='text-center my-5 flex flex-col gap-4'>
-        <h1 className="text-2xl font-bold">Usuários</h1>
-        <AddUser />
+    <div>
+      <div className="bg-base-100">
+        <div className="navbar max-w-4xl mx-auto px-3">
+          <div className="navbar-start">
+            <a className="text-2xl font-bold">Filmes</a>
+          </div>
+          <div className="navbar-end gap-3">
+            <a className="btn">Filmes</a>
+            <a className="btn">Usuários</a>
+          </div>
+        </div>
       </div>
-      <UserList users={users.data} />
-    </main>
+
+      <main className='max-w-4xl mx-auto mt-4 px-3'>
+        <div className='text-center my-5 flex flex-col gap-4'>
+          <h1 className="text-2xl font-bold">Usuários</h1>
+          <AddUser />
+        </div>
+        <UserList users={users.data} />
+      </main>
+    </div>
   );
 }
